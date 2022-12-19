ac5259fe8f19cda6609623e0f93b97dbdb4e2f6a
DanielZamora <zdaniel647@gmail.com>
2022-12-18 23:11:36 -0600

    arreglo

--- src/Components/Pagina2.jsx
+++ src/Components/Pagina2.jsx
@@ -10,7 +10,37 @@ function Pagina2() {
         <h2 className='me' >"Me, myself, my life, my hobbies, everything"</h2>
       </div>
         <div className='pp2'>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit, explicabo dolorem veniam aut voluptatum unde. Harum totam tenetur officia, et sequi illum, nostrum architecto sit asperiores magni at distinctio velit. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repudiandae, architecto tenetur? Necessitatibus neque ducimus dolorem similique voluptatum hic reprehenderit fugiat ad odit error repudiandae quas, in voluptates dolores voluptatem aliquam.</p>
+          <p className='bio'>
+            My name is Daniel Virgen Zamora, in this autobiography I want to tell
+             you how my passion and interest for technology started, since my childhood 
+             I have always been interested and curious about how things around me work, 
+             these feelings increased when we talk about technology.
+          </p>   
+          <p className='bio'> I still remember myself
+              almost destroying all my toys, just to know how and why they emitted light,
+              made sounds or had movement, soon I discovered that it was due to the wonders of
+              technology, thanks to that since I was little I had basic knowledge about electricity,
+              magnetism and electric motors.
+          </p> 
+          
+          <p className='bio'>I have always been fascinated by how and why
+              things in this life work, which has led me to have an analytical, self-critical 
+              and self-taught personality, all these factors eventually led me to have curiosity 
+              about the Internet, my interest was no longer only in the hardware but had crossed to
+              the software.
+          </p> 
+          
+          <p className='bio'>that same curiosity led me to discover an app, which luckily offered 
+              a bootcamp to learn web programming, I entered with the minimum knowledge in this 
+              topic, I felt nervous about what I would experience, but today I completely thank 
+              that decision I made, this experience has been completely gratifying for me, it has
+              allowed me to learn many and very useful new skills, that will help me to develop
+              professionally, I have learned how to develop web pages through HTML and languages 
+              like CSS and Javascript, working environment such as React and structures such as
+              Vite, I have made friends who have nourished my learning, the truth is I feel
+              very happy for what I have achieved in my time in the bootcamp and I feel
+              completely prepared to start applying my skills in a professional career.
+          </p>
         </div>
         <div className="firma">
 
